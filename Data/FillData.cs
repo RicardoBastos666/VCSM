473541fbdc390bcaf69d89d4b845b3bfa10afd79
JoaoPereiraVicaima <Joao.Pereira@vicaima.pt>
2024-02-20 16:04:46 +0000

    Adição de FillData e FillItem

---
+++ Data/FillData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSM.Data
+{
+    public class FillData
+    {
+        public int ContainerLength
+        {
+            get
+            {
+                return 11850;
+            }
+        }
+        public int ContainerWidth
+        {
+            get
+            {
+                return 2300;
+            }
+        }
+    }
+}
---
+++ Data/FillItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSM.Data
+{
+    public class FillItem
+    {
+        public int Order { get; set; }
+        public int Position { get; set; }
+        public string Mode { get; set; }
+        public string Description { get; set; }
+    }
+}
